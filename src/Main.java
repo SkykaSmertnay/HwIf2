87a897546d47693c30fcbd720d59328c2f2585bf
SkykaSmertnay <g925730@gmail.com>
2025-05-18 17:25:33 +0300

    Fix 1

--- src/Main.java
+++ src/Main.java
@@ -93,7 +93,7 @@ public class Main {
         {
             System.out.println("Доставки нет");
         }
-        // Задание №5
+        // Задание №5.
         System.out.println("\n Задание №5");
         int monthNumber;
         System.out.println("Введите номер меяца: ");
